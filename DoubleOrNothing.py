c1452144d56638eaf65ed4ba551c211fa92a9cda
Alex-Manson <ama9652@stacmail.net>
2024-02-08 14:54:57 +1300

    Create DoubleOrNothing.py

---
+++ DoubleOrNothing.py
@@ -0,0 +1,5 @@
+''' This program takes a number as input and doubles it. '''
+
+# Takes number input as a float and then prints double the number.
+num = float(input("Please enter a number. This program will then double it. "))
+print(f"Your newly doubled number is {num*2}!")
