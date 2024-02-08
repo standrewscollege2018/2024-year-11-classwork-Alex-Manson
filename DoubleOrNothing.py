53c4da1b849abd7ed5b6f827567a814fc99a3ea7
Alex-Manson <ama9652@stacmail.net>
2024-02-08 15:12:39 +1300

    Update DoubleOrNothing.py

--- DoubleOrNothing.py
+++ DoubleOrNothing.py
@@ -2,10 +2,12 @@
 
 # Takes number input as a float and then prints double the number.
 keep_asking = True
-while keep_asking == True:
+
+while keep_asking:
     try:
         num = float(input("Please enter a number. This program will then double it. "))
-        print(f"Your newly doubled number is {num*2}!")
         keep_asking = False
     except ValueError:
         print("Next time, enter a whole number or decimal, don't be irrational... ")
+
+print(f"Your newly doubled number is {num*2}!")
