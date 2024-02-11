ae8cd9f4ef7966a854c960e8892ab1f822a70fc5
Alex-Manson <ama9652@stacmail.net>
2024-02-12 09:31:31 +1300

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -1,5 +1,7 @@
 '''This program takes two integers as inputs and returns the product of the two.'''
 keep_asking = True
+def multiply(a, b):
+    return a * b
 while keep_asking:
     print("This program takes two integers as inputs and returns the product of the two.")
     try:
@@ -8,5 +10,4 @@ while keep_asking:
         keep_asking = False
     except ValueError:
         print("Next time, enter an integer, don't be irrational... ")
-product = num1*num2
-print(f"The product of your two integers is {product}!")
+print(f"The product of your two integers is {multiply(num1, num2)}!")
