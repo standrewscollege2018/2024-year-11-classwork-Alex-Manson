2c04ffcf24ee977e7e0a5fa7bf39d7049b599bd4
Alex-Manson <ama9652@stacmail.net>
2024-02-27 19:46:27 +1300

    e

---
+++ rentalcars.py
@@ -0,0 +1,2 @@
+''''''
+cars = [["Suzuki Van", 2, True], ["Toyota Corolla", 4, True], ["Honda CRV", , True], ["Suzuki Swift", , True], ["Mitsubishi Airtrek", , True], ["Nissan DC Ute", , True], ["", , True], ["", , True], ["", , True]]
